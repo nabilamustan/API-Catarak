855d13c5b0cc08b8d88a340a7d975bb3b51fab60
nabilamustan <127601032+nabilamustan@users.noreply.github.com>
2023-12-22 02:07:17 +0800

    Update connectdb.js

--- config/connectdb.js
+++ config/connectdb.js
@@ -1,25 +1,66 @@
+const express = require('express');
 const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDb = require('./connectdb');
 
-const serviceAccount = require('../UserAuth/serviceAccountKey.json');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://your-firebase-database-url.firebaseio.com',
 });
 
-const db = admin.firestore();
+// Registrasi
+app.post('/register', async (req, res) => {
+  const { fullname, email, password } = req.body;
 
-async function saveUserData(uid, fullname, username, email) {
-  try {
-    await db.collection('users').doc(uid).set({
-      fullname,
-      username,
-      email,
-     
-    });
-  } catch (error) {
-    console.error('Error saving user data:', error);
+  // Validasi input
+  if (!fullname || !email || !password) {
+    return res.status(400).json({ error: 'Oops! All fields are required.' });
   }
-}
 
-module.exports = { db, saveUserData };
+  try {
+    // Buat pengguna di Firebase Authentication
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: fullname,
+    });
+
+    // Simpan data pengguna di Firestore
+    await connectDb.saveUserData(userRecord.uid, fullname, email);
+
+    res.status(200).json({ message: 'Hooray! User registered successfully.', uid: userRecord.uid });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ error: 'Oh no! ' + error.message });
+  }
+});
+
+// Login
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // Validasi input
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Uh-oh! Email and password are required.' });
+  }
+
+  admin.auth().getUserByEmail(email)
+    .then(userRecord => {
+      res.status(200).json({ message: 'Great! Login successful.', uid: userRecord.uid, fullname: userRecord.displayName });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Oops! ' + error.message });
+    });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Awesome! Server is running on port ${PORT}`);
+});
+
+module.exports = app;
