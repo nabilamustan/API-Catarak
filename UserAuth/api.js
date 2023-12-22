99391521159c547ae4b719102a0718fefaf4b389
nabilamustan <127601032+nabilamustan@users.noreply.github.com>
2023-12-22 14:41:26 +0800

    Update api.js

--- UserAuth/api.js
+++ UserAuth/api.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -15,48 +16,46 @@ admin.initializeApp({
 });
 
 // Registrasi
-app.post('/register', (req, res) => {
-  const { fullname, email, password } = req.body;
+app.post('/register', async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
 
-  // Validasi input
-  if (!fullname || !email || !password) {
-    return res.status(400).json({ error: 'Oops! All fields are required.' });
-  }
+    // Validasi input
+    if (!fullname || !email || !password) {
+      return res.status(400).json({ error: 'Oops! All fields are required.' });
+    }
 
-  admin.auth().createUser({
-    email,
-    password,
-    displayName: fullname,
-  })
-  .then(userRecord => {
-    
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: fullname,
+    });
+
     res.status(200).json({ message: 'Hooray! User registered successfully.', uid: userRecord.uid });
-  })
-  .catch(error => {
+  } catch (error) {
     res.status(500).json({ error: 'Oh no! ' + error.message });
-  });
+  }
 });
 
 // Login
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  // Validasi input
-  if (!email || !password) {
-    return res.status(400).json({ error: 'Uh-oh! Email and password are required.' });
+    // Validasi input
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Uh-oh! Email and password are required.' });
+    }
+
+    const userRecord = await admin.auth().getUserByEmail(email);
+
+    res.status(200).json({ message: 'Great! Login successful.', uid: userRecord.uid, fullname: userRecord.displayName });
+  } catch (error) {
+    res.status(500).json({ error: 'Oops! ' + error.message });
   }
-
-  admin.auth().getUserByEmail(email)
-    .then(userRecord => {
-    
-      res.status(200).json({ message: 'Great! Login successful.', uid: userRecord.uid, fullname: userRecord.displayName });
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Oops! ' + error.message });
-    });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => {
   console.log(`Awesome! Server is running on port ${PORT}`);
 });
