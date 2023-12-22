f45f0938d77f5aedb631cb8ad32504f32b3cd067
nabilamustan <127601032+nabilamustan@users.noreply.github.com>
2023-12-22 14:42:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const userAuthAPI = require('./UserAuth/api');
 const uploadAPI = require('./blob/upload');
-const imageController = require('./controller/imageController');
+const imageController = require('./blob/imageController');
 
 app.use('/user', userAuthAPI);
 app.use('/upload', uploadAPI);
 
 // Endpoint untuk deteksi katarak
 app.post('/detectCataract', async (req, res) => {
-  const imageUrl = req.body.imageUrl;
-
   try {
-    const result = await imageController.detectCataract(imageUrl);
-    res.status(200).json({ message: 'Cataract detection successful', result });
+    // Ambil data gambar dari body request
+    const imageData = req.body.imageData;
+
+    // Pemrosesan gambar sesuai kebutuhan model.json
+    const result = await imageController.detectCataract(imageData);
+
+    // Menentukan threshold untuk menetapkan apakah gambar memiliki katarak atau tidak
+    const threshold = 0.5; // Sesuaikan dengan nilai threshold yang sesuai
+    const isCataract = result > threshold;
+
+    res.status(200).json({ message: 'Cataract detection successful', isCataract });
   } catch (error) {
     res.status(500).json({ error: 'Error in cataract detection: ' + error.message });
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Awesome! Server is running on port ${PORT}`);
 });
